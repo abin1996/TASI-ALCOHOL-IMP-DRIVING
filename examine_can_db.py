5c308c94f3c7eefb0d2d910b85c06fcd8765c526
Abin Mathew <abinmth@gmail.com>
2023-10-04 11:40:28 -0400

    Alcohol DBC

---
+++ examine_can_db.py
@@ -0,0 +1,114 @@
+import argparse
+import sys
+# import canlib as canlib
+from canlib import kvadblib,Frame,canlib
+
+INDENT = ' ' * 4
+
+
+def print_db(db):
+    print('DATABASE')
+    print(db.name)
+    for line in db_lines(db):
+        print(INDENT + line)
+
+
+def adef_lines(adef):
+    yield 'type: ' + type(adef).__name__
+    yield 'definition: ' + str(adef.definition)
+    yield 'owner: ' + str(adef.owner)
+
+
+def attr_lines(attrib):
+    yield str(attrib.name) + ' = ' + str(attrib.value)
+
+
+def db_lines(db):
+    yield 'flags: ' + str(db.flags)
+    yield 'protocol: ' + str(db.protocol)
+    yield ''
+
+    yield 'ATTRIBUTE DEFINITIONS'
+    for adef in db.attribute_definitions():
+        yield str(adef.name)
+        for line in adef_lines(adef):
+            yield INDENT + line
+    yield ''
+
+    yield 'MESSAGES'
+    for message in db:
+        yield str(message.name)
+        for line in msg_lines(message):
+            yield INDENT + line
+    yield ''
+
+
+def enum_lines(enums):
+    for name, val in enums.items():
+        yield str(name) + ' = ' + str(val)
+
+
+def msg_lines(message):
+    yield 'id: ' + str(message.id)
+    yield 'flags: ' + str(message.flags)
+    yield 'dlc: ' + str(message.dlc)
+    yield 'comment: ' + str(message.comment)
+    yield ''
+
+    yield 'ATTRIBUTES'
+    for attr in message.attributes():
+        for line in attr_lines(attr):
+            yield line
+    yield ''
+
+    yield 'SIGNALS'
+    for signal in message:
+        yield str(signal.name)
+        for line in sig_lines(signal):
+            yield INDENT + line
+    yield ''
+
+
+def sig_lines(signal):
+    for name in ('type', 'byte_order', 'mode', 'size', 'scaling', 'limits', 'unit', 'comment'):
+        yield name + ': ' + str(getattr(signal, name))
+    yield ''
+
+    try:
+        enums = signal.enums
+    except AttributeError:
+        pass
+    else:
+        yield 'ENUMERATIONS'
+        for line in enum_lines(enums):
+            yield line
+        yield ''
+
+    yield 'ATTRIBUTES'
+    for attr in signal.attributes():
+        for line in attr_lines(attr):
+            yield line
+    yield ''
+
+
+def examine_database(db_name):
+    with kvadblib.Dbc(filename=db_name) as db:
+        # print_db(db)
+        for message in db.messages():
+            # print(message)
+            frame = Frame(id_=0x320, data=[1], flags=canlib.MessageFlag.EXT)
+            bmsg = db.interpret(frame)
+            print(bmsg._message)
+            print(bmsg._frame)
+            for bsig in bmsg:
+                print('┃',bsig)
+ 
+
+if __name__ == '__main__':
+    # parser = argparse.ArgumentParser(description=sys.modules[__name__].__doc__)
+    # parser.add_argument(
+    #     'db', type=str, help='The dbc database file to examine.'
+    # )
+    # args = parser.parse_args()
+
+    examine_database('/home/iac_user/data_collection_scripts/dadss_breath_sensor.dbc')
--- read_can_msgs.py
+++ read_can_msgs.py
@@ -1,23 +1,49 @@
 #send_msg
 # The CANlib library is initialized when the canlib module is imported. To be
 # able to send a message, Frame also needs to be installed.
-from canlib import canlib, Frame
+from canlib import canlib, Frame, kvadblib
 import time
+
+
+
+def printframe(db, frame):
+    try:
+        bmsg = db.interpret(frame)
+    except kvadblib.KvdNoMessage:
+        print("<<< No message found for frame with id %s >>>" % frame.id)
+        print(frame.id)
+        return
+
+    if not bmsg._message.dlc == bmsg._frame.dlc:
+        print(
+            "<<< Could not interpret message because DLC does not match for frame with id %s >>>"
+            % frame.id
+        )
+        print("\t- DLC (database): %s" % bmsg._message.dlc)
+        print("\t- DLC (received frame): %s" % bmsg._frame.dlc)
+        return
+
+    msg = bmsg._message
+
+
+
+
 # Firstly, open two CAN channels, one to send the message and one to receive.
 # Note that there needs to be a channel to receive, as otherwise the message
 # can not be sent. In this example the channels are named ch_a and ch_b. To
 # open the channels call on the openChannel method inside of canlib and, as an
 # input put in channel=0 and channel=1. Where 0 and 1 represents the two
 # CANlib channels 0 and 1.
-# ch_a = canlib.openChannel(channel=1)
-ch_b = canlib.openChannel(channel=0)
+db = kvadblib.Dbc(filename='/home/iac_user/data_collection_scripts/dadss_breath_sensor.dbc')
+ch_a = canlib.openChannel(channel=1)
+# ch_b = canlib.openChannel(channel=0)
 
 # After opening the channel, we need to set the bus parameters. Some
 # interfaces keep their params from previous programs. This can cause problems
 # if the params are different between the interfaces/channels. For now we will
 # use setBusParams() to set the canBitrate to 250K.
-# ch_a.setBusParams(canlib.canBITRATE_250K)
-ch_b.setBusParams(canlib.canBITRATE_500K)
+ch_a.setBusParams(canlib.canBITRATE_500K)
+# ch_b.setBusParams(canlib.canBITRATE_500K)
 
 # The next step is to Activate the CAN chip for each channel (ch_a and ch_b in
 # this example) use .busOn() to make them ready to receive and send messages.
@@ -29,22 +55,39 @@ ch_b.setBusParams(canlib.canBITRATE_500K)
 # this example, the CANFrame is named frame. Then send the message by calling on
 # the channel that will act as the sender and use .write() with the CANFrame
 # # as input. In this example ch_a will act as sender.
-ch_b.busOn()
-frame = Frame(id_=0x010D, data=[], flags=canlib.MessageFlag.STD )
-ch_b.write(frame)
-ch_b.busOff()
+ch_a.busOn()
+frame = Frame(id_=0x320, data=[1], flags=canlib.MessageFlag.EXT )
+ch_a.write(frame)
+ch_a.busOff()
 
 # To make sure the message was sent we will attempt to read the message. Using
 # timeout, only 500 ms will be spent waiting to receive the CANFrame. If it takes
 # longer the program will encounter a timeout error. read the CANFrame by calling
 # .read() on the channel that receives the message, ch_b in this example. To
 # then read the message we will use print() and send msg as the input.
+ch_a.busOn()
 while True:
-    ch_b.busOn()
-    msg = ch_b.read(timeout=500)
-    ch_b.busOff()
-    print(msg)
-    time.sleep(2)
+    msg = ch_a.read(timeout=500)
+    if msg.id == 0x30F:
+        print("1:")
+        print(msg)
+        printframe(db, msg)
+        # break
+    # if msg.id == 783:
+    #     print("2:")
+    #     print(msg)
+        # break
+    if msg.id == 0x31F:
+        print("3:")
+        print(msg)
+        printframe(db, msg)
+        # break
+    # if msg.id == 799:
+    #     print("4:")
+    #     print(msg)
+        # break
+ch_a.busOff()
+
 
 # After the message has been sent, received and read it is time to inactivate
 # the CAN chip. To do this call .busOff() on both channels that went .busOn()
@@ -52,8 +95,8 @@ while True:
 
 
 # Lastly, close all channels with close() to finish up.
-# ch_a.close()
-ch_b.close()
+ch_a.close()
+# ch_b.close()
 
 # Depending on the situation it is not always necessary or preferable to go of
 # the bus with the channels and, instead only use close(). But this will be

6fdb63ccb1a8302ddc64a6885c013316e7ff7143
Abin Mathew <abinmth@gmail.com>
2023-10-10 13:45:29 -0400

    brac test fixed

--- record_brac_can_messages.py
+++ record_brac_can_messages.py
@@ -4,6 +4,7 @@
 from canlib import canlib, Frame, kvadblib
 import pandas as pd
 import datetime
+import time
 RESULTS = []
 
 def get_frame_data(db, frame, start_time, print_to_stdout=False):
@@ -46,8 +47,11 @@ def get_frame_data(db, frame, start_time, print_to_stdout=False):
     result_ready = False 
     test_failed = False
     test_count = None
+    test_status = None
     for bsig in bmsg:
         cur_signals.append([bsig.name,bsig.value,bsig.unit])
+        if bsig.name == 'e_Status':
+            test_status = bsig.value
         if bsig.name == 'e_Status' and bsig.value == 6:
             result_ready = True
         if bsig.name == 'e_Status' and bsig.value == 5:
@@ -55,13 +59,16 @@ def get_frame_data(db, frame, start_time, print_to_stdout=False):
         if bsig.name == 'n_BrAC_Count':
             test_count = bsig.value
     
-    cur_message = {'time':cur_time,'frame_id':cur_frame_id,'msg_name':cur_msg_name,'signals':cur_signals, 'result_ready':result_ready, 'test_count':test_count, 'test_failed':test_failed}
+    cur_message = {'time':cur_time,'frame_id':cur_frame_id,'msg_name':cur_msg_name,'signals':cur_signals, 'result_ready':result_ready, 'test_count':test_count, 'test_failed':test_failed, 'test_status':test_status}
 
     return cur_message
 
+def save_to_csv(filename, all_frame_data):
+    df = pd.DataFrame(all_frame_data)
+    df.to_csv(filename,columns=['time','frame_id','msg_name','signals'])
 
 
-def record_brac(filename):
+def record_brac(filename, start_time):
     db = kvadblib.Dbc(filename='/home/iac_user/data_collection_scripts/dadss_breath_sensor_fixed.dbc')
     ch_a = canlib.openChannel(channel=1)
     ch_a.setBusParams(canlib.canBITRATE_500K)
@@ -69,91 +76,84 @@ def record_brac(filename):
     print("Starting BRAC test")
     frame = Frame(id_=800, data=bytearray(b'\x01\x00\x00\x00\x00\x00\x00\x00'), flags=canlib.MessageFlag.STD)
     ch_a.write(frame)
+    time.sleep(0.1)
+    frame = Frame(id_=800, data=bytearray(b'\x00\x00\x00\x00\x00\x00\x00\x00'), flags=canlib.MessageFlag.STD)
+    ch_a.write(frame)
 
     all_frame_data = []
-    start_time = datetime.datetime.now()
-    latest_brac_count = None
-    print("Blow into the sensor")
-    num_of_results = 0
+    
     failed_tests = set()
+    success_tests = set()
     break_loop = False
     channel_closed = False
+    print_to_stdout = False
+
+    num_of_tests_done = 0
+    latest_estatus = 0
+    number_of_failed_tests = 0
+    number_of_successful_tests = 0
+    print("Blow into the sensor")
     while True:
         try:
-            msg = ch_a.read(timeout=500)
+            msg = ch_a.read(timeout=-1)
+
             if msg.id == 783:
-                # print("Status:")
-                # print(msg)
-                decoded_frame = get_frame_data(db, msg, start_time, print_to_stdout=False)
+                decoded_frame = get_frame_data(db, msg, start_time, print_to_stdout=print_to_stdout)
                 all_frame_data.append(decoded_frame)
-                if not latest_brac_count:
-                    print("setting latest_brac_count {}".format(decoded_frame['test_count']))
-                    latest_brac_count = decoded_frame['test_count']
+                if latest_estatus != decoded_frame['test_status']:
+                    print("Status: {}".format(decoded_frame['test_status']))
+                    latest_estatus = decoded_frame['test_status']
+                
+                if decoded_frame['test_count'] != num_of_tests_done:
+                    num_of_tests_done = decoded_frame['test_count']
+                    print("Total Tests Done: {}".format(num_of_tests_done))   
                 
                 if decoded_frame['test_failed'] and decoded_frame['test_count'] not in failed_tests:
                     failed_tests.add(decoded_frame['test_count'])
-                    if num_of_results > 0:
-                        num_of_results -= 1
+                    number_of_failed_tests += 1
                     print("Test FAILED")
-                    print("Tests Done: {}".format(num_of_results))
+                    print("Number of Failed Tests: {}".format(number_of_failed_tests))
 
-
-                elif decoded_frame['test_count'] > latest_brac_count:
-                    num_of_results += 1
-                    print("Tests Done: {}".format(num_of_results))
-                    latest_brac_count = decoded_frame['test_count']
-                    if num_of_results == 2:
+                if decoded_frame['result_ready'] and decoded_frame['test_count'] not in success_tests:
+                    success_tests.add(decoded_frame['test_count'])
+                    number_of_successful_tests += 1
+                    print("Number of Succesful Tests Done: {}".format(number_of_successful_tests))
+                    frame = Frame(id_=800, data=bytearray(b'\x01\x00\x00\x00\x00\x00\x00\x00'), flags=canlib.MessageFlag.STD)
+                    ch_a.write(frame)
+                    time.sleep(0.1)
+                    frame = Frame(id_=800, data=bytearray(b'\x00\x00\x00\x00\x00\x00\x00\x00'), flags=canlib.MessageFlag.STD)
+                    ch_a.write(frame)
+                    if number_of_successful_tests == 2:
                         break_loop = True
 
-                if decoded_frame['result_ready']:
-                    num_of_results += 1
-                    print("Tests Done: {}".format(num_of_results))
-                    latest_brac_count = decoded_frame['test_count']
-                    if num_of_results == 2:
-                        break_loop = True
-
-                
-                # break
             if msg.id == 799:
-                # print("Results:")
-                # print(msg)
-                decoded_frame = get_frame_data(db, msg, start_time, print_to_stdout=False)
+                decoded_frame = get_frame_data(db, msg, start_time, print_to_stdout=print_to_stdout)
                 all_frame_data.append(decoded_frame)
-                if num_of_results == 2:
+                if number_of_successful_tests == 2:
                         if break_loop:
                             break
-                    # printframe(db, msg)
-                    # break
-                # break
+
         except canlib.CanNoMsg:
                 # if ticktime is not None:
-                print("No Message to Read")
+                print("Waiting for message")
         except KeyboardInterrupt:
             print("Stopping")
-            frame = Frame(id_=800, data=bytearray(b'\x04\x00\x00\x00\x00\x00\x00\x00'), flags=canlib.MessageFlag.STD)
+            frame = Frame(id_=800, data=bytearray(b'\x00\x00\x00\x00\x00\x00\x00\x00'), flags=canlib.MessageFlag.STD)
             ch_a.write(frame)
             ch_a.busOff()
             ch_a.close()
             channel_closed = True
-            df = pd.DataFrame(all_frame_data)
-            df.to_csv(filename)
+            save_to_csv(filename, all_frame_data)
             break
-    df = pd.DataFrame(all_frame_data)
-    df.to_csv(filename)
-        # except Exception as e:
-        #     print("Stopping")
-        #     ch_a.busOff()
-        #     ch_a.close()
-        #     break
-            #Saving all_frame_data to a csv file using pandas dataframe
-            # df = pd.DataFrame(all_frame_data)
-            # df.to_csv(filename)
+
+    save_to_csv(filename, all_frame_data)
     if not channel_closed:
-        frame = Frame(id_=800, data=bytearray(b'\x04\x00\x00\x00\x00\x00\x00\x00'), flags=canlib.MessageFlag.STD)
+        frame = Frame(id_=800, data=bytearray(b'\x00\x00\x00\x00\x00\x00\x00\x00'), flags=canlib.MessageFlag.STD)
         ch_a.write(frame)
         ch_a.busOff()
         ch_a.close()
 
 if __name__ == "__main__":
-    filename ='/home/iac_user/data_collection_scripts/brac_test.csv'
+    start_time = datetime.datetime.now()
+    filename ='/home/iac_user/data_collection_scripts/brac_test/brac_{}.csv'.format(start_time.strftime('%d-%m-%y_%H-%M-%S'))
     record_brac(filename)

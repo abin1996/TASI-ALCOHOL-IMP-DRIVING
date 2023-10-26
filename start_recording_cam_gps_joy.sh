15e112c0c2952baadec9bdc92c6e9f20c5fa7122
Abin Mathew <abinmth@gmail.com>
2023-10-25 21:24:43 -0400

    command error

--- start_recording_cam_gps_joy.sh
+++ start_recording_cam_gps_joy.sh
@@ -17,6 +17,6 @@ rosbag record --split --duration 60 -o $TARGET_FOLDER/images1/images1.bag -b 0 /
 rosbag record --split --duration 60 -o $TARGET_FOLDER/images2/images2.bag -b 0 /camera2/usb_cam2/image_raw/compressed &
 rosbag record --split --duration 60 -o $TARGET_FOLDER/images3/images3.bag -b 0 /camera3/usb_cam3/image_raw/compressed &
 rosbag record --split --duration 60 -o $TARGET_FOLDER/images4/images4.bag -b 0 /camera4/usb_cam4/image_raw/compressed &
-python /home/iac_user/data_collection_scripts/record_driving_can_messages.py --filename $TARGET_FOLDER/can/can_messages_$NOW.csv --db /home/iac_user/data_collection_scripts/446w_iupui_fixed.dbc &
+python3 /home/iac_user/data_collection_scripts/record_driving_can_messages.py --filename $TARGET_FOLDER/can/can_messages_$NOW.csv --db /home/iac_user/data_collection_scripts/446w_iupui_fixed.dbc &
 rosbag record --split --duration 60 -o $TARGET_FOLDER/gps/gps.bag -b 0 /tcpfix /tcptime /tcpvel
 
